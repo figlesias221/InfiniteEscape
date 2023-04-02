22bc682495afa2e2ccf11d53393fa56e81e910be
Federico Iglesias <federicoiglesias@192.168.1.2>
2023-04-02 16:51:18 -0300

    circle/cirlce collision detection

---
+++ Assets/Scripts/CircleCollider.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleCollider : MonoBehaviour
+{
+    public float radius;
+    
+    public bool CheckCollision(Vector2 circlePos, float circleRadius)
+    {
+        Vector2 centerPos = transform.position;
+        float distance = Vector2.Distance(circlePos, centerPos);
+        return distance <= radius + circleRadius;
+    }
+}
+
